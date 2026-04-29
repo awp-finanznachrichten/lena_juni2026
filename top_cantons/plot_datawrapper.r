5a62c9750bdd93b67d0b41779c7cf967f666c80c
awp-finanznachrichten <sw@awp.ch>
2026-04-29 10:58:28 +0200

    commit from Rstudio

---
+++ top_cantons/plot_datawrapper.r
@@ -0,0 +1,23 @@
+
+
+
+
+#chart id
+id_fr <- "jjjYB"
+id_de <- "6q0ZA"
+id_it <- "u4Rfa"
+
+
+
+
+
+dw_data_to_chart(tabelle_fr,id_fr)
+dw_publish_chart(id_fr)
+
+
+dw_data_to_chart(tabelle_de,id_de)
+dw_publish_chart(id_de)
+
+
+dw_data_to_chart(tabelle_it,id_it)
+dw_publish_chart(id_it)
---
+++ top_flop/id.r
@@ -0,0 +1,50 @@
+# FR
+obj_1_top_fr_id  <- "b6w1U"
+obj_1_flop_fr_id <- "yuXg3"
+
+obj_2_top_fr_id  <- "N5EPN"
+obj_2_flop_fr_id <- "Gbd3L"
+
+obj_3_top_fr_id  <- "iOHcE"
+obj_3_flop_fr_id <- "CxB1Z"
+
+obj_4_top_fr_id  <- "b51TV"
+obj_4_flop_fr_id <- "ghurA"
+
+obj_5_top_fr_id  <- "bKE7p"
+obj_5_flop_fr_id <- "bP1w2"
+
+
+# DE
+obj_1_top_de_id  <- "Dx1fO"
+obj_1_flop_de_id <- "a7eVM"
+
+obj_2_top_de_id  <- "i1RhM"
+obj_2_flop_de_id <- "Bsp3H"
+
+obj_3_top_de_id  <- "VqlJy"
+obj_3_flop_de_id <- "cJ9NA"
+
+obj_4_top_de_id  <- "Ir09o"
+obj_4_flop_de_id <- "ic1Hw"
+
+obj_5_top_de_id  <- "k5NEY"
+obj_5_flop_de_id <- "tFhxa"
+
+
+# IT
+obj_1_top_it_id  <- "UTWq0"
+obj_1_flop_it_id <- "2LB0K"
+
+obj_2_top_it_id  <- "Pkwo0"
+obj_2_flop_it_id <- "WENIo"
+
+obj_3_top_it_id  <- "ROaJd"
+obj_3_flop_it_id <- "80Rpw"
+
+obj_4_top_it_id  <- "Urowj"
+obj_4_flop_it_id <- "JMav4"
+
+obj_5_top_it_id  <- "P86Hn"
+obj_5_flop_it_id <- "z4Xpy"
+ 
