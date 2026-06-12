a592e7c6d08b0f479f7c904b9a78e35d748a1051
awp-finanznachrichten <sw@awp.ch>
2026-06-12 15:13:00 +0200

    commit from Rstudio

--- Config/set_constants.R
+++ Config/set_constants.R
@@ -4,7 +4,7 @@ voting_date <- "2026-06-14"
 date_voting <- "20260614"
 
 #Save texts? Simulation? Default FALSE
-simulation <- TRUE
+simulation <- FALSE
 save_texts <- FALSE
 
 #Output for special Einzugsgebiete? Default FALSE
